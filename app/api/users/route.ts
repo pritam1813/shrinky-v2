63fb538bfc4123d976156eaa38ed3bd93d796428
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-02-07 00:29:03 +0530

    Protected API routes

--- app/api/users/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const allUsers = await prisma.users.findMany();
-  return NextResponse.json(allUsers, { status: 201 });
-}
